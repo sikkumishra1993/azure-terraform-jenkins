3d07f0909e70b9a5a50d59c58176441e160b9cd9
sikkumishra1993 <49817049+sikkumishra1993@users.noreply.github.com>
2025-03-25 22:36:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+backend "azurerm" {
+    resource_group_name  = "terraform-jenkins"
+    storage_account_name = "smiterraformjenkins"
+    container_name       = "tfstate"
+    key                  = "${terraform.workspace}/terraform.tfstate"
+  }
   required_version = ">= 1.0.0"
 }
 
